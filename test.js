0d6e3b400c46ab3b6e049b74f88e50fe356167ae
Harshit Saini <harshitsaini8063@gmail.com>
2026-07-31 13:34:47 +0530

    Commit messages

---
+++ test.js
@@ -0,0 +1,10 @@
+import {sum,add} from "./demo5.js";
+console.log("sum=",sum(1));
+console.log("sum=",sum(20,30));
+console.log("sum=",sum(1,2,3,4,5,6));
+console.log("sum=",sum(34,56,78,90));
+
+console.log("add=",add(1));
+console.log("add=",add(20,30));
+console.log("add=",add(1,2,3,4,5,6));
+console.log("add=",add(34,56,78,90));
